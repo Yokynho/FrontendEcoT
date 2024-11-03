204092e39a15f57924ad7d8ca4b4bfe6fded35f3
Jorge <jolaechea80@gmail.com>
2024-11-02 19:26:13 -0500

    Creacion del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    UsuariosComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { CreaeditausuarioComponent } from './components/usuarios/creaeditausuario/creaeditausuario.component';
+import { ControlesComponent } from './components/controles/controles.component';
+import { CreaeditacontrolComponent } from './components/controles/creaeditacontrol/creaeditacontrol.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'usuarios',
+        component:UsuariosComponent,
+        children:[
+            {
+                path:'nuevo',
+                component: CreaeditausuarioComponent,
+            },
+            {
+                path:'ediciones/:id', component: CreaeditausuarioComponent,
+            },
+        ],
+    },
+    {
+        path:'controles',
+        component:ControlesComponent,
+        children:[
+            {
+                path:'nuevo',
+                component:CreaeditacontrolComponent,
+            },
+            {
+                path:'ediciones/:id', component:CreaeditacontrolComponent,
+            },
+        ],
+    },
+];
