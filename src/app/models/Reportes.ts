62c3cf8168c236d0440fb190bf55fd65ed255463
Jorge <jolaechea80@gmail.com>
2024-11-05 13:15:18 -0500

    Creacion del Models Reportes

---
+++ src/app/models/Reportes.ts
@@ -0,0 +1,9 @@
+export class Reportes{
+    idReportes:number=0
+    titulo:string=""
+    descripcion:string =""
+    fecha_creacion:Date=new Date(Date.now())
+    tipo:string =""
+    respuesta:string =""
+
+}
