bbe152f821e8430ae865b0b13dafb1ab8a7d4bfc
Yokynho16 <u202212560@upc.edu.pe>
2024-11-06 17:19:01 -0500

    Creacion del model Pagos

---
+++ src/app/models/Pagos.ts
@@ -0,0 +1,10 @@
+import{Cotizaciones}from "./Cotizaciones"
+export class Pagos{
+    idPagos:number=0
+    monto:number=0
+    fecha_pago:Date=new Date(Date.now())
+    estado:string=""
+    fecha_vencimiento:Date=new Date(Date.now())
+    co:Cotizaciones=new Cotizaciones()
+
+}
