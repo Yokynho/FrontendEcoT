c2014b8662e336e9f7687480dd883dc3249def55
Yokynho16 <u202212560@upc.edu.pe>
2024-11-06 17:45:11 -0500

    Crear Component de Vehiculos

---
+++ src/app/components/vehiculos/creaeditavehiculos/creaeditavehiculos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creaeditavehiculos',
+  standalone: true,
+  imports: [],
+  templateUrl: './creaeditavehiculos.component.html',
+  styleUrl: './creaeditavehiculos.component.css'
+})
+export class CreaeditavehiculosComponent {
+
+}
---
+++ src/app/components/vehiculos/listarvehiculos/listarvehiculos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarvehiculos',
+  standalone: true,
+  imports: [],
+  templateUrl: './listarvehiculos.component.html',
+  styleUrl: './listarvehiculos.component.css'
+})
+export class ListarvehiculosComponent {
+
+}
