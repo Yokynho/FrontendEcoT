3dfae50067b73c712956ab77d4eecb6e0172c345
06KJ <kim605j@gmail.com>
2024-11-10 22:57:30 -0500

    Creación del component listarmetodospagos y una correción

--- src/app/components/metodospago/listarmetodospago/listarmetodospago.component.ts
+++ src/app/components/metodospago/listarmetodospago/listarmetodospago.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
+import { MetodosPago } from '../../../models/MetodosPago';
+import { MetodospagoService } from '../../../services/metodospago.service';
 
 @Component({
   selector: 'app-listarmetodospago',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule,MatIconModule,MatButtonModule,RouterModule],
   templateUrl: './listarmetodospago.component.html',
   styleUrl: './listarmetodospago.component.css'
 })
-export class ListarmetodospagoComponent {
+export class ListarmetodospagoComponent implements OnInit{
+  dataSource: MatTableDataSource<MetodosPago> = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'c1',
+    'c2',
+    'c3',
+    'c4',
+    'c5',
+    'c6',
+    'accion01',
+    'accion02',
+  ];
+  constructor(private mS: MetodospagoService) {}
+  ngOnInit(): void {
 
+    this.mS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+    this.mS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
+  eliminar(id: number) {
+    this.mS.delete(id).subscribe((data) => {
+      this.mS.list().subscribe((data) => {
+        this.mS.setList(data);
+      });
+    });
+  }
 }
