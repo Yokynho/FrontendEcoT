7954796b8246d98d757efd7803493e76820024d0
06KJ <kim605j@gmail.com>
2024-11-11 21:49:02 -0500

    Agregación del paginator a las tablas quejas y metodo de pago

--- src/app/components/metodospago/listarmetodospago/listarmetodospago.component.ts
+++ src/app/components/metodospago/listarmetodospago/listarmetodospago.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { MetodosPago } from '../../../models/MetodosPago';
 import { MetodospagoService } from '../../../services/metodospago.service';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listarmetodospago',
   standalone: true,
-  imports: [MatTableModule,MatIconModule,MatButtonModule,RouterModule],
+  imports: [MatTableModule,MatIconModule, MatPaginator,MatButtonModule,RouterModule],
   templateUrl: './listarmetodospago.component.html',
   styleUrl: './listarmetodospago.component.css'
 })
 export class ListarmetodospagoComponent implements OnInit{
   dataSource: MatTableDataSource<MetodosPago> = new MatTableDataSource();
+  totalItems: number = 0;//Manejar la cantidad
   displayedColumns: string[] = [
     'c1',
     'c2',
@@ -26,13 +28,16 @@ export class ListarmetodospagoComponent implements OnInit{
     'accion02',
   ];
   constructor(private mS: MetodospagoService) {}
+  @ViewChild(MatPaginator) paginator!:MatPaginator;
   ngOnInit(): void {
 
     this.mS.list().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator=this.paginator;//agregado
     });
     this.mS.getList().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator=this.paginator;//agregado
     });
   }
   eliminar(id: number) {
