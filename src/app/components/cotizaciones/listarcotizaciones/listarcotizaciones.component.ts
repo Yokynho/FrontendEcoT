590f855e417e9bb4e7287b4ee72bc85a1f6e1c43
Abdali15 <abdalicampos.h@gmail.com>
2024-11-15 23:55:37 -0500

    paginator

--- src/app/components/cotizaciones/listarcotizaciones/listarcotizaciones.component.ts
+++ src/app/components/cotizaciones/listarcotizaciones/listarcotizaciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,10 +6,12 @@ import { RouterModule } from '@angular/router';
 import { Cotizaciones } from '../../../models/Cotizaciones';
 import { CotizacionesService } from '../../../services/cotizaciones.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-listarcotizaciones',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [MatPaginator,MatTableModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './listarcotizaciones.component.html',
   styleUrl: './listarcotizaciones.component.css'
 })
@@ -17,7 +19,8 @@ export class ListarcotizacionesComponent implements OnInit{
   dataSource: MatTableDataSource<Cotizaciones>=new MatTableDataSource();
 
   displayedColumns:string[]=['c1','c2','c3','c4','accion01','accion02',]
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private cS:CotizacionesService,
     private snackBar: MatSnackBar
   ){}
@@ -48,5 +51,9 @@ export class ListarcotizacionesComponent implements OnInit{
         this.cS.setList(data);
       });
     });
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 }
