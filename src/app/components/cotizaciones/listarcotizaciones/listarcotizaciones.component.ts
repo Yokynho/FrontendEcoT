7739b3df470984816fa90d1f65426897a412f6a9
Jorge <jolaechea80@gmail.com>
2024-11-17 19:51:09 -0500

    Correcion de la entidad pagos

--- src/app/components/cotizaciones/listarcotizaciones/listarcotizaciones.component.ts
+++ src/app/components/cotizaciones/listarcotizaciones/listarcotizaciones.component.ts
@@ -9,18 +9,20 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { LoginService } from '../../../services/login.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-listarcotizaciones',
   standalone: true,
-  imports: [MatPaginator,MatTableModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [MatPaginator,MatTableModule, MatIconModule, MatButtonModule, RouterModule, CommonModule],
   templateUrl: './listarcotizaciones.component.html',
   styleUrl: './listarcotizaciones.component.css'
 })
 export class ListarcotizacionesComponent implements OnInit{
   dataSource: MatTableDataSource<Cotizaciones>=new MatTableDataSource();
 
-  displayedColumns:string[]=['c1','c2','c3','c4','accion01','accion02',]
+  displayedColumns:string[]=['c1','c2','c3','c4']
   username:string=''
+  role: string = '';
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -33,18 +35,36 @@ export class ListarcotizacionesComponent implements OnInit{
   ngOnInit(): void { 
 
     this.username=this.loginService.showUsername();
+    this.role=this.loginService.showRole();
 
 
-    this.cS.listByUsername(this.username).subscribe((data) => {
-      this.dataSource = new MatTableDataSource(data);
+    if(this.isAgricultor()){
+      this.cS.list().subscribe((data)=>{
+        this.dataSource=new MatTableDataSource(data);
       if (this.dataSource.data.length === 0) {
         this.mostrarMensajeSinDatos();
-      }
-    });
+        }
+      });
+    }else{
+      this.cS.listByUsername(this.username).subscribe((data) => {
+        this.dataSource = new MatTableDataSource(data);
+        if (this.dataSource.data.length === 0) {
+          this.mostrarMensajeSinDatos();
+        }
+      });
+    }
+
+
     
     this.cS.getList().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
     });
+    if(this.isDistribuidor()){
+      this.displayedColumns.push('accion01');
+    }
+    if(this.isDistribuidor()){
+      this.displayedColumns.push('accion02');
+    }
   }
 
   mostrarMensajeSinDatos() {
@@ -64,5 +84,17 @@ export class ListarcotizacionesComponent implements OnInit{
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+
+  isAgricultor() {
+    return this.role === 'AGRICULTOR';
+  }
+
+  isDistribuidor() {
+    return this.role === 'DISTRIBUIDOR';
+  }
+  isAdministrador(){
+    return this.role === 'ADMINISTRADOR';
   }
 }
