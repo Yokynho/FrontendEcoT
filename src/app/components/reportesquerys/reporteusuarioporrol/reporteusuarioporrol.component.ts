a8ab586d210d88e9cea78157d3ca140404da6fa9
Abdali15 <abdalicampos.h@gmail.com>
2024-11-17 19:23:11 -0500

    a

--- src/app/components/reportesquerys/reporteusuarioporrol/reporteusuarioporrol.component.ts
+++ src/app/components/reportesquerys/reporteusuarioporrol/reporteusuarioporrol.component.ts
@@ -29,7 +29,6 @@ export class ReporteusuarioporrolComponent implements OnInit{
           backgroundColor: ['#ff5733', '#ffbd33', '#33b5ff', '#ff1493', '#00b894', '#e17055', '#0984e3', '#fd79a8', '#f39c12', '#9b59b6'],  // Colores más vibrantes
           borderColor: '#1a1a1a',  // Borde oscuro para mayor contraste
           borderWidth: 1,  // Borde más grueso para hacer que las barras resalten
-          fill: true,  // Para rellenar el área debajo de las barras (para un gráfico tipo área)
           
         },
       ];
